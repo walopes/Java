db01d39954c500ac2c82ef50a6f388296eaf1d72
Willian A. Lopes <wlopes@alunos.utfpr.edu.br>
2018-02-11 01:59:23 -0200

    Luta class
    The Luta class has been added to the project

---
+++ UltraEmojiCombat/src/ultraemojicombat/Luta.java
@@ -0,0 +1,30 @@
+
+package ultraemojicombat;
+
+
+public class Luta {
+    
+    /* Atributos */
+    private String desafiado;
+    private String desafiante;
+    private int rounds;
+    private int aprovada;
+    
+    /* Métodos */
+    
+    public Luta()
+    {
+        ;
+    }
+    
+    public void marcarLuta()
+    {
+        ;
+    }
+    
+    public void lutar()
+    {
+        ;
+    }
+    
+}
