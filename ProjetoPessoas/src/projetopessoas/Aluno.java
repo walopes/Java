a8612a61e8fdce3bf7f2dc9a8b4af45139996a0a
Willian A. Lopes <wlopes@alunos.utfpr.edu.br>
2018-02-15 01:05:58 -0200

    ProjetoPessoa added
    The project ProjetoPessoa, which uses heritage, has been added

---
+++ ProjetoPessoas/src/projetopessoas/Aluno.java
@@ -0,0 +1,10 @@
+
+package projetopessoas;
+
+/**
+ *
+ * @author walopes
+ */
+public class Aluno {
+    
+}
---
+++ ProjetoPessoas/src/projetopessoas/ProjetoPessoas.java
@@ -0,0 +1,17 @@
+
+package projetopessoas;
+
+/**
+ *
+ * @author walopes
+ */
+public class ProjetoPessoas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
